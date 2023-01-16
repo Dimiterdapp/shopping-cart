e06d50bfb8b34299874ccd9ecde3a70cc2fba763
PC <PC@DESKTOP-2V5KFPK>
2023-01-16 13:59:34 +0200

    Add project files.

---
+++ Shop-cart/Knockout/jsonFile.js
@@ -0,0 +1,123 @@
+﻿var items = [
+    {
+        "Categories": [
+            {
+                "Products": [
+                    {
+                        ID: 1,
+                        "Name": "Iphone 12",
+                        price: 1559.50
+                    },
+                    {
+                        ID: 2,
+                        "Name": "Iphone 14",
+                        price: 2279.26
+                    }
+                ],
+
+                ID: 1,
+                "Name": "Apple"
+            },
+            {
+                "Products": [
+                    {
+                        ID: 1,
+                        "Name": "Galaxy S21",
+                        price: 1099.87
+                    },
+                    {
+                        ID: 2,
+                        "Name": "Galaxy S22",
+                        price: 1400.00
+                    }
+                ],
+
+                ID: 2,
+                "Name": "Samsung"
+            }
+        ],
+        ID: 1,
+        "Name": "Smartphones"
+    },
+    {
+        "Categories": [
+            {
+                "Products": [
+                    {
+                        ID: 1,
+                        "Name": "Acer",
+                        price: 199.00
+                    },
+                    {
+                        ID: 2,
+                        "Name": "Philips",
+                        price: 206.22
+                    }
+                ],
+
+                ID: 1,
+                "Name": "Monitors"
+            },
+            {
+                "Products": [
+                    {
+                        ID: 1,
+                        "Name": "Xerox",
+                        price: 429.99
+                    },
+                    {
+                        ID: 2,
+                        "Name": "Phaser",
+                        price: 199.10
+                    }
+                ],
+
+                ID: 2,
+                "Name": "Printers"
+            }
+        ],
+        ID: 2,
+        "Name": "PC peripherials"
+    },
+    {
+        "Categories": [
+            {
+                "Products": [
+                    {
+                        ID: 1,
+                        "Name": "Bocai Bracele",
+                        price: 126.22
+                    },
+                    {
+                        ID: 2,
+                        "Name": "Moissanite Ring",
+                        price: 140.44
+                    }
+                ],
+
+                ID: 1,
+                "Name": "Diamond Jewelry"
+            },
+            {
+                "Products": [
+                    {
+                        ID: 1,
+                        "Name": "Baltany",
+                        price: 410.74
+                    },
+                    {
+                        ID: 2,
+                        "Name": "Heimdallr",
+                        price: 428.90
+                    }
+                ],
+
+                ID: 2,
+                "Name": "Watches"
+            }
+        ],
+
+        ID: 3,
+        "Name": "Jewelry"
+    }
+];
